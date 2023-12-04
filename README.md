6773957a8fdebe35d3cf8a3f236422126f227c35
Vinicius Ferraz Campos Florentino <vinicius.ferraz@gmail.com>
2023-12-03 23:31:28 -0300

    v2

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # Link
 https://access.redhat.com/documentation/en-us/red_hat_build_of_keycloak/22.0/html-single/operator_guide/index
 
+# Pre
+- Install Keycloak Operator
+- Red Hat OpenShift GitOps
+
 # TODO
 - script ansible
   - Definir o HOST no inicio
   - Exibir no final usuario e senha do admin do KC (secret <KC_NAME>-initial-admin)
+
+# Plus
+- ArgoCD
+  - https://openshift-gitops-server-openshift-gitops.apps.cluster-vqgg6.dynamic.opentlc.com/
--- prereq/create.sh
+++ prereq/create.sh
@@ -1,4 +1,4 @@
-openssl req -subj '/CN=test.keycloak.org/O=Test Keycloak./C=US' -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out certificate.pem
+openssl req -subj '/CN=keycloak-rhbk.apps.cluster-vqgg6.dynamic.opentlc.com/O=Test Keycloak./C=US' -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out certificate.pem
 oc create secret tls example-tls-secret --cert certificate.pem --key key.pem
 oc create secret generic keycloak-db-secret \
   --from-literal=username=postgres \
